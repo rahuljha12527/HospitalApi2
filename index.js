4ece874118ce31e4b0a936b4e51f10781b1426b6
rahuljha661 <rahuljha12527@gmail.com>
2020-10-12 16:14:38 +0530

    add index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const app=express();
+const port=8000;
+
+
+const bodyParser=require('body-parser');
+//middleware for connecting form  data into text
+app.use(express.urlencoded());
+app.use(bodyParser.json());
+
+app.use('/',require('./routes'));
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+})
